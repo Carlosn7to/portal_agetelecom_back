5bfaae2272f4c887d65a8078a46121838c7b8caf
Carlos Alberto M. Da F. Neto <carlos.neto@tote.local>
2023-10-14 09:23:22 -0300

    alteracao regra rv

--- app/Http/Controllers/AgeRv/_aux/sales/Stars.php
+++ app/Http/Controllers/AgeRv/_aux/sales/Stars.php
@@ -172,7 +172,8 @@ class Stars
                 }
 
                 // Se o mês do cadastro do contrato for AGOSTO, executa esse bloco.
-            } elseif (Carbon::parse($item->data_contrato) >= Carbon::parse('2022-08-01')) {
+            } elseif (Carbon::parse($item->data_contrato) >= Carbon::parse('2022-08-01') &&
+                Carbon::parse($item->data_contrato) < Carbon::parse('2023-08-01')) {
 
                 // Verifica qual é o plano e atribui a estrela correspondente.
                 if (str_contains($item->plano, 'PLANO 1 GIGA FIDELIZADO + DEEZER + HBO MAX + DR. AGE')) {
@@ -230,7 +231,7 @@ class Stars
                 } elseif (str_contains($item->plano, 'PLANO 800 MEGA NÃO FIDELIZADO')) {
                     $this->stars += 0;
                 }
-            } elseif (Carbon::parse($item->data_contrato) >= Carbon::parse('2023-11-01')) {
+            } elseif (Carbon::parse($item->data_contrato) >= Carbon::parse('2023-08-01')) {
                 // Verifica qual é o plano e atribui a estrela correspondente.
                 if (str_contains($item->plano, 'PLANO 1 GIGA FIDELIZADO + DEEZER PREMIUM + DIRECTV GO')) {
                     $this->stars += 20;
