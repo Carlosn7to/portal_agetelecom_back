4028fa98797856961b349a093002f1efefbe6267
Carlos Alberto M. Da F. Neto <carlos.neto@tote.local>
2023-10-24 15:50:00 -0300

    alteração regua

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
 
         $schedule->command('update:tables')->dailyAt('3:00');
-        $schedule->command('send:billing-rule')->weekdays()->dailyAt('10:37');
+        $schedule->command('send:billing-rule')->weekdays()->dailyAt('9:30');
 
     }
 
