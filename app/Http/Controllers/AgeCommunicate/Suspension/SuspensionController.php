ccf6251318ccbe6f3ce792dbc8a8b3194776499d
Carlos Alberto M. Da F. Neto <carlos.neto@tote.local>
2023-10-25 10:04:33 -0300

    adicao email 120d

--- app/Http/Controllers/AgeCommunicate/Suspension/SuspensionController.php
+++ app/Http/Controllers/AgeCommunicate/Suspension/SuspensionController.php
@@ -47,7 +47,7 @@ class SuspensionController extends Controller
 
     private function sendEmail()
     {
-        $to = 'carlos.neto@agetelecom.com.br';
+        $to = 'suspensao@agetelecom.com.br';
 
         $mail = Mail::mailer('portal')->to($to)->send(new SendSuspencion($this->getData()));
 
