85e4688b3346d858768bf20e05ff98ffde98e7e3
Carlos Alberto M. Da F. Neto <carlos.neto@tote.local>
2023-12-07 09:52:48 -0300

    regua

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
 
         $schedule->command('update:tables')->dailyAt('3:00');
-        $schedule->command('send:billing-rule')->weekdays()->dailyAt('10:00');
+        $schedule->command('send:billing-rule')->weekdays()->dailyAt('11:00');
         $schedule->command('send:suspension')->weekdays()->everyThreeHours();
         $schedule->command('send:blockedClients')->weekdays()->dailyAt('17:00');
         $schedule->command('export:order')->everyFiveMinutes();
